efe4953691a137dba317e63c4b79686247a556b0
Paul Dreik <github@pauldreik.se>
2025-02-02 21:43:45 +0100

    compile as c++ instead of c

--- src/lemac.h
+++ src/lemac.h
@@ -3,10 +3,6 @@
 #include <immintrin.h>
 #include <stdint.h>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 typedef struct {
   __m128i S[9];
 } state;
@@ -20,7 +16,3 @@ typedef struct {
 void lemac_init(context* ctx, const uint8_t k[]);
 void lemac_MAC(context* ctx, const uint8_t* nonce, const uint8_t* m,
                size_t mlen, uint8_t* tag);
-
-#ifdef __cplusplus
-} // extern "C"
-#endif
