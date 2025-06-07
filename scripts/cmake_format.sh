f636b9f742c8ef36f950f27aa77676d0694285d2
Paul Dreik <github@pauldreik.se>
2025-06-07 09:22:53 +0200

    fix cmake format script

--- scripts/cmake_format.sh
+++ scripts/cmake_format.sh
@@ -7,8 +7,6 @@ set -eu
 cd "$(dirname "$0")"/..
 
 # apt install cmake-format
-cmake-format -i \
-  CMakeLists.txt \
-  benchmark/CMakeLists.txt \
-  test/CMakeLists.txt \
-  ;
+git ls-files \
+  | grep "CMakeLists.txt" \
+  | xargs cmake-format -i
